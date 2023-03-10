65fe02aab35dc7dd9ac2c38b6a9833dbc9df339f
atah <habibiatah@gmail.com>
2023-03-10 08:04:11 -0800

    project in progress

--- src/projectsData.js
+++ src/projectsData.js
@@ -16,62 +16,138 @@ const projects=[
                 liveLink:'https://atah-habibi-porfolio.netlify.app/'
             },
             
-            {
-                projectName:'My portfolio',
-                imgLink:'../images/color-flipper.png',
-                githubLink:'https://github.com/Atahhabibi/ATAH-PORTFOLIO',
-                info:
-                'This is my portfolio design by me using html, css and Javascript and other helpful css tools such as media queries for   different screen sizes and full responsive, you can view project live by clicking the live button and download the source code ',
-            
-                liveLink:'https://atah-habibi-porfolio.netlify.app/'
-            },
-
-            {
-                projectName:'My portfolio',
-                imgLink:'../images/kara-store.png',
-                githubLink:'https://github.com/Atahhabibi/ATAH-PORTFOLIO',
-                info:
-                'This is my portfolio design by me using html, css and Javascript and other helpful css tools such as media queries for   different screen sizes and full responsive, you can view project live by clicking the live button and download the source code ',
-            
-                liveLink:'https://atah-habibi-porfolio.netlify.app/'
-            },
-
-
-
         ],
 
         miniScale:[
 
 
             {
-                projectName:'My portfolio',
-                imgLink:'../images/atah-screen.png',
-                githubLink:'https://github.com/Atahhabibi/ATAH-PORTFOLIO',
+                id:1,
+                projectName:'color Flipper',
+                imgLink:'../images/color-flipper.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/01-Color-Flipper',
                 info:
-                'This is my portfolio design by me using html, css and Javascript and other helpful css tools such as media queries for   different screen sizes and full responsive, you can view project live by clicking the live button and download the source code ',
+                'This is is small color-flipper prjoect and help in genrating random numbers and randomly choose    different index of array to create hexdecimal color and then set it as background color',
             
-                liveLink:'https://atah-habibi-porfolio.netlify.app/'
+                liveLink:'https://color-background-flipper.netlify.app'
             },
 
             {
-                projectName:'My portfolio',
-                imgLink:'../images/atah-screen.png',
-                githubLink:'https://github.com/Atahhabibi/ATAH-PORTFOLIO',
+                id:2,
+                projectName:'random dad Joke',
+                imgLink:'../images/rand-2.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/21-Dad-Jokes',
                 info:
-                'This is my portfolio design by me using html, css and Javascript and other helpful css tools such as media queries for   different screen sizes and full responsive, you can view project live by clicking the live button and download the source code ',
+                'This is random dad joke generator by using third person API, every time user click on random button and new joke is fetch and displayed in the screen. Please visit the live by clicking live icon in the middle',
             
-                liveLink:'https://atah-habibi-porfolio.netlify.app/'
+                liveLink:'https://random-dade-joke-project.netlify.app'
             },
 
             {
-                projectName:'My portfolio',
-                imgLink:'../images/atah-screen.png',
-                githubLink:'https://github.com/Atahhabibi/ATAH-PORTFOLIO',
+                id:3,
+                projectName:'background images',
+                imgLink:'../images/bg-2.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/15-Slider',
                 info:
-                'This is my portfolio design by me using html, css and Javascript and other helpful css tools such as media queries for   different screen sizes and full responsive, you can view project live by clicking the live button and download the source code ',
+                'This is background images viewer you can choose any background images you want from given images you need to click next to view the next image. for more info please visit the live sit by clicking the live icon',
             
-                liveLink:'https://atah-habibi-porfolio.netlify.app/'
+                liveLink:'https://background-images-site.netlify.app/'
             },
+
+
+            {
+                id:4,
+                projectName:'Counter',
+                imgLink:'../images/count.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/16-Counter',
+                info:
+                'This is counter project, it increaes and decrease the given number and reset it to zero by clicking the speific button given ,the purpose of creating this project is to understand creating function constructer and classes ',
+            
+                liveLink:'https://counter-min-project.netlify.app/'
+            },
+
+            {
+                id:5,
+                projectName:'Gallery',
+                imgLink:'../images/gallery.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/17-Gallery',
+                info:
+                'This is gallery project, the reason of creating this project is to understand use of classes and function constructer, this project show us different categroies of images for example nature and city by opening modal viewer',
+            
+                liveLink:'https://gallery-site-project.netlify.app/'
+            },
+            {
+                id:6,
+                projectName:'Tabs',
+                imgLink:'../images/tab.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/11-Tabs',
+                info:
+                'This is tab project contains different tab you can view different image and information by clicking on right tab , this project was created to undertand the use of tab in website',
+            
+                liveLink:'https://tabs-mini-project.netlify.app/'
+            },
+            {
+                id:7,
+                projectName:'scroll',
+                imgLink:'../images/scroll.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/10-Scroll',
+                info:
+                'This is scroll project, the main focus of this project is to understand the scroll after user click the navbar. Bring the scroll exactly in the above the right section and adjust the height of navbar to show the links in small screen devices ',
+            
+                liveLink:''
+            },
+            {
+                id:8,
+                projectName:'Dark theme',
+                imgLink:'../images/theme-2.png',
+                githubLink:'https://github.com/Atahhabibi/ATAH-PRACTICE/tree/main/19-Dark-mode',
+                info:
+                'This is theme mode project , it help the user to change bacground of the article and also moment.js library is covered which format the data and time into different form  and for more info visit github respository',
+            
+                liveLink:'https://change-theme-site.netlify.app/'
+            },
+            {
+                id:3,
+                projectName:'',
+                imgLink:'../images/',
+                githubLink:'',
+                info:
+                '',
+            
+                liveLink:''
+            },
+            {
+                id:3,
+                projectName:'',
+                imgLink:'../images/',
+                githubLink:'',
+                info:
+                '',
+            
+                liveLink:''
+            },
+            {
+                id:3,
+                projectName:'',
+                imgLink:'../images/',
+                githubLink:'',
+                info:
+                '',
+            
+                liveLink:''
+            },
+            {
+                id:3,
+                projectName:'',
+                imgLink:'../images/',
+                githubLink:'',
+                info:
+                '',
+            
+                liveLink:''
+            },
+
+            
 
 
 
